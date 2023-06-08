ca1c1f7dfeedc0128a65957d8344843a26171b6b
Edgar Martinez <edgar.agustin.martinez@gmail.com>
2023-06-08 18:52:33 +0000

    Add actions colors
    Actions store their own color independent from the theme

--- src/core/messages/api/Action.ts
+++ src/core/messages/api/Action.ts
@@ -1,18 +1,23 @@
+type ActionColor = 'Primary' | 'Secondary' | 'Danger';
+
 export default interface Action {
   name: string
   text: string
+  color: ActionColor
   onlyOwnerInteraction: boolean
 }
 
-export const YesAndNoAction = [
+export const YesAndNoAction: Action[] = [
   {
     name: 'yes',
     text: 'yes',
-    onlyOwnerInteraction: false
+    onlyOwnerInteraction: false,
+    color: 'Primary'
   },
   {
     name: 'no',
     text: 'no',
-    onlyOwnerInteraction: false
+    onlyOwnerInteraction: false,
+    color: 'Danger'
   }
 ]
