723ac5c63363115b798d4864e442a9d310294b70
Edgar Martinez <edgar.agustin.martinez@gmail.com>
2023-06-20 22:25:10 +0000

    Make sample test more educational

--- test/sample.ts
+++ test/sample.ts
@@ -1,9 +1,13 @@
 import { assert } from 'chai'
 
+// Use functions and not arrow functions (() =>)
+// https://mochajs.org/#arrow-functions
 describe('Array', function () {
   describe('#indexOf()', function () {
     it('should return -1 when the value is not present', function () {
       assert.equal([1, 2, 3].indexOf(4), -1)
     })
+    // You can describe more use cases for the member indexof() here...
   })
+  // You can describe tests for more members of Array here...
 })
