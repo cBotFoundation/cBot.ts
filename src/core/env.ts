3b32239ea6d9340a147cfc2d0270880c2b2fc850
Edgar Martinez <edgar.agustin.martinez@gmail.com>
2023-07-23 20:20:56 -0500

    Fix env.ts warnings

--- src/core/env.ts
+++ src/core/env.ts
@@ -1,18 +1,18 @@
-import { z } from  'zod'
-import "dotenv/config"
+import { z } from 'zod'
+import 'dotenv/config'
 
-const env_vars = z.object({
-    PORT: z.string(),
-    RUN_COMMAND_DEPLOYER: z.string(),
-    DISCORD_CLIENT_ID: z.string(),
-    DISCORD_GUILD_ID: z.string(),
-    DISCORD_BOT_TOKEN: z.string()
+const ENV_VARS = z.object({
+  PORT: z.string(),
+  RUN_COMMAND_DEPLOYER: z.string(),
+  DISCORD_CLIENT_ID: z.string(),
+  DISCORD_GUILD_ID: z.string(),
+  DISCORD_BOT_TOKEN: z.string()
 })
 
-env_vars.parse(process.env)
+ENV_VARS.parse(process.env)
 
 declare global {
-    namespace NodeJS {
-        interface ProcessEnv extends z.infer<typeof env_vars> {} 
-    }
+  namespace NodeJS {
+    interface ProcessEnv extends z.infer<typeof ENV_VARS> {}
+  }
 }
